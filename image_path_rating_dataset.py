f2627292e9649dca54a59a2c7a29b1a60756ddb3
Uri Berger <uri.berger2@mail.huji.ac.il>
2024-06-22 21:16:07 +1000

    Fixed a small bug

--- image_path_rating_dataset.py
+++ image_path_rating_dataset.py
@@ -2,7 +2,7 @@ from human_rating_dataset import HumanRatingDataset
 from PIL import Image
 
 class ImagePathRatingDataset(HumanRatingDataset):
-    def get_image(image_data):
+    def get_image(self, dataset_name, image_data):
         return Image.open(image_data['file_path']).convert("RGB")
     
     def get_file_path(self, dataset_name, image_data):
