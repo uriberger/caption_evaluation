69cd17408c6958966475c059c2dc21b3db485a7c
Uri Berger <uri.berger2@mail.huji.ac.il>
2024-06-23 23:23:30 +1000

    Added some functions to polaris dataset

--- polaris_dataset.py
+++ polaris_dataset.py
@@ -11,6 +11,12 @@ class PolarisDataset(HumanRatingDataset):
         super(PolarisDataset, self).__init__()
         self.hash2image_obj = None
 
+    def get_name(self):
+        return 'polaris'
+    
+    def get_file_name2iid_func(self, dataset_name):
+        return lambda x: int(x)
+
     def collect_data(self):
         dataset = load_dataset('yuwd/Polaris')
         data = {}
