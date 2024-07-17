7c4056b8831af2c879516e03804a443f27856004
Uri Berger <uri.berger2@mail.huji.ac.il>
2024-07-17 13:53:13 +1000

    Adjusted reformulations dataset to multiple rating mode

--- reformulations_dataset.py
+++ reformulations_dataset.py
@@ -40,7 +40,7 @@ class ReformulationsDataset(ImagePathRatingDataset):
                     'captions': []
                 }
             cur_ind = len(data[cur_dataset][image_id]['captions'])
-            data[cur_dataset][image_id]['captions'].append({'caption': sample['question'], 'human_rating': 0, 'tag': 'before', 'pair': cur_ind+1, 'automatic_metrics': {}})
-            data[cur_dataset][image_id]['captions'].append({'caption': sample['answer'], 'human_rating': 1, 'tag': 'after', 'pair': cur_ind, 'automatic_metrics': {}})
+            data[cur_dataset][image_id]['captions'].append({'caption': sample['question'], 'human_ratings': [0], 'tag': 'before', 'pair': cur_ind+1, 'automatic_metrics': {}})
+            data[cur_dataset][image_id]['captions'].append({'caption': sample['answer'], 'human_ratings': [1], 'tag': 'after', 'pair': cur_ind, 'automatic_metrics': {}})
 
         self.data = data
