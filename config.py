dc941f06e47b424cfb9da2080305e1702d4a8e2a
Uri Berger <uri.berger2@mail.huji.ac.il>
2024-07-25 06:59:00 +1000

    Improved the composite dataset code

---
+++ config.py
@@ -0,0 +1,7 @@
+flickr30k_image_dir_path = '/cs/labs/oabend/uriber/datasets/flickr30/images'
+flickr30k_json_path = '/cs/labs/oabend/uriber/datasets/flickr30/karpathy/dataset_flickr30k.json'
+flickr8k_image_dir_path = '/cs/snapless/gabis/uriber/caption_evaluation/pacscore/datasets/flickr8k/images'
+flickr8k_json_path = 'pacscore/datasets/flickr8k/flickr8k.json'
+coco_image_dir_path = '/cs/labs/oabend/uriber/datasets/COCO'
+coco_json_path = '/cs/snapless/gabis/uriber/CLIP_prefix_caption/dataset_coco.json'
+composite_path = '/cs/labs/oabend/uriber/datasets/AMT_eval'
--- datasets/composite_dataset.py
+++ datasets/composite_dataset.py
@@ -4,17 +4,14 @@ import csv
 import json
 from Levenshtein import distance
 import numpy as np
+from config import flickr30k_image_dir_path, \
+    flickr8k_image_dir_path, \
+    coco_image_dir_path, \
+    composite_path, \
+    flickr30k_json_path, \
+    flickr8k_json_path, \
+    coco_json_path
 
-iid2file_path = {
-    'flickr8k': lambda x: f'/cs/labs/oabend/uriber/datasets/flickr30/images/{x}.jpg',
-    'flickr30k': lambda x: f'/cs/labs/oabend/uriber/datasets/flickr30/images/{x}.jpg',
-    'coco': lambda x: f'/cs/labs/oabend/uriber/datasets/COCO/{x.split("_")[1]}/{x}'
-}
-file_name2iid = {
-    'flickr8k': lambda x: int(x),
-    'flickr30k': lambda x: int(x),
-    'coco': lambda x: int(x.split('_')[-1])
-}
 entry2iid = {
     'flickr8k': lambda x: x[27].split('/')[-1].split('_')[0],
     'flickr30k': lambda x: x[27].split('/')[-1].split('.')[0],
@@ -26,56 +23,53 @@ dataset2caption_num = {
     'coco': 4
 }
 dataset_to_file_name = {'flickr8k': '8k', 'flickr30k': '30k', 'coco': 'coco'}
-human_rating_dir = '/cs/labs/oabend/uriber/datasets/AMT_eval'
+human_rating_dir = composite_path
 
 class CompositeDataset(ImagePathRatingDataset):
     def get_name(self):
         return 'composite'
     
-    def get_file_name2iid_func(self, dataset_name):
-        return file_name2iid[dataset_name]
-    
     def collect_data(self, remove_gt_candidates=False, remove_gt_candidates_from_ref_set=True):
-        datasets = ['flickr8k', 'flickr30k', 'coco']
+        splits = ['flickr8k', 'flickr30k', 'coco']
 
-        for dataset in datasets:
-            self.collect_data_for_dataset(dataset, remove_gt_candidates, remove_gt_candidates_from_ref_set)
+        for split in splits:
+            self.collect_data_for_split(split, remove_gt_candidates, remove_gt_candidates_from_ref_set)
 
         res_str = 'Collected '
         first = True
-        for dataset in datasets:
+        for split in splits:
             if first:
                 first = False
             else:
                 res_str += ', '
-            res_str += f'{len(self.data[dataset])} images and {len([x for outer in self.data[dataset].values() for x in outer["captions"]])} captions for {dataset}'
+            res_str += f'{len(self.data[split])} images and {len([x for outer in self.data[split].values() for x in outer["captions"]])} captions for {split}'
         print(res_str, flush=True)
 
-    def collect_data_for_dataset(self, dataset_name, remove_gt_candidates, remove_gt_candidates_from_ref_set):
-        human_rating_file_path = os.path.join(human_rating_dir, f'{dataset_to_file_name[dataset_name]}_correctness.csv')
+    def collect_data_for_split(self, split_name, remove_gt_candidates, remove_gt_candidates_from_ref_set):
+        human_rating_file_path = os.path.join(human_rating_dir, f'{dataset_to_file_name[split_name]}_correctness.csv')
 
-        if dataset_name == 'flickr8k':
-            with open('pacscore/datasets/flickr8k/flickr8k.json', 'r') as fp:
+        if split_name == 'flickr8k':
+            with open(flickr8k_json_path, 'r') as fp:
                 json_data = json.load(fp)
             data = {int(x[0].split('_')[0]): {
                 'references': x[1]['ground_truth'],
-                'file_path': f'/cs/snapless/gabis/uriber/caption_evaluation/pacscore/datasets/flickr8k/images/{x[1]["image_path"].split("/")[1]}',
+                'file_path': os.path.join(flickr8k_image_dir_path, f'{x[1]["image_path"].split("/")[1]}'),
                 'captions': []
                 } for x in json_data.items()}
-        elif dataset_name == 'flickr30k':
-            with open('/cs/labs/oabend/uriber/datasets/flickr30/karpathy/dataset_flickr30k.json', 'r') as fp:
+        elif split_name == 'flickr30k':
+            with open(flickr30k_json_path, 'r') as fp:
                 json_data = json.load(fp)['images']
             data = {int(x['filename'].split('.')[0]): {
                 'references': [y['raw'] for y in x['sentences']],
-                'file_path': f'/cs/labs/oabend/uriber/datasets/flickr30/images/{x["filename"]}',
+                'file_path': os.path.join(flickr30k_image_dir_path, f'{x["filename"]}'),
                 'captions': []
                 } for x in json_data}
-        elif dataset_name == 'coco':
-            with open('/cs/snapless/gabis/uriber/CLIP_prefix_caption/dataset_coco.json', 'r') as fp:
+        elif split_name == 'coco':
+            with open(coco_json_path, 'r') as fp:
                 json_data = json.load(fp)['images']
             data = {x['cocoid']: {
                 'references': [y['raw'] for y in x['sentences']],
-                'file_path': f'/cs/labs/oabend/uriber/datasets/COCO/{x["filepath"]}/{x["filename"]}',
+                'file_path': os.path.join(coco_image_dir_path, f'{x["filepath"]}/{x["filename"]}'),
                 'captions': []
                 } for x in json_data}
 
@@ -86,14 +80,14 @@ class CompositeDataset(ImagePathRatingDataset):
                 if first:
                     first = False
                     continue
-                image_id_str = entry2iid[dataset_name](sample)
+                image_id_str = entry2iid[split_name](sample)
                 if len(image_id_str) == 0:
                     continue
                 image_id = int(image_id_str)
                 if image_id not in data:
-                    print(f'Missing image in {dataset_name}')
+                    print(f'Missing image in {split_name}')
                     continue
-                cap_num = dataset2caption_num[dataset_name]
+                cap_num = dataset2caption_num[split_name]
                 for i in range(cap_num):
                     if i == 3:
                         continue # All previous studies only used the first 3 captions
@@ -101,9 +95,6 @@ class CompositeDataset(ImagePathRatingDataset):
                         # First caption in this dataset is one of the references, like in the CLIPScore paper: ignore
                         continue
                     data[image_id]['captions'].append({'caption': sample[28+i], 'human_ratings': [int(sample[28+cap_num+i])], 'automatic_metrics': {}})
-                    # if i == 0:
-                    #     # If we didn't ignore, tell the ref based metrics to ignore the same reference
-                    #     data[image_id]['captions'][0]['ignore_refs'] = [np.argmin([distance(sample[28+i], ref) for ref in data[image_id]['references']])]
 
         data = {x[0]: x[1] for x in data.items() if len(x[1]['captions']) > 0}
 
@@ -114,4 +105,4 @@ class CompositeDataset(ImagePathRatingDataset):
                 ref_set = data[image_id]['references']
                 data[image_id]['references'] = [ref_set[i] for i in range(len(ref_set)) if i != cand_ind_in_refs]
 
-        self.data[dataset_name] = data
+        self.data[split_name] = data
